d71ac3c487058bb27e4c85a1eaa96cb13c68f069
liviaguimaraes92 <liviaguimaraes92@gmail.com>
2021-07-20 19:00:55 -0300

    blog pessoal front end

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor (
+    private http: HttpClient
+    ) { }
+
+
+  entrar(userLogin: UserLogin): Observable<UserLogin>{
+    return this.http.post<UserLogin> (`${environment.server}/usuarios/logar`, userLogin)
+  }
+
+  cadastrar(user: User): Observable<User>{
+    return this.http.post<User>(`${environment.server}/usuarios/cadastrar`, user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`${environment.server}/usuarios/${id}`)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if(environment.token != ""){
+      ok = true
+    }
+
+    return ok
+    
+  }
+
+  adm(){
+    let ok: boolean = false
+
+    if(environment.tipo = 'adm'){
+      ok = true
+    }
+
+    return ok
+  }
+
+}
